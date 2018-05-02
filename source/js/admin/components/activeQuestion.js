11f5ef0ce666e9bdd55accd75f75104cfb137af5
Aaron Ladage <aladage@gmail.com>
2018-05-01 23:20:14 -0500

    active event, game and question admin stuff working

---
+++ source/js/admin/components/activeQuestion.js
@@ -0,0 +1,73 @@
+// Utils
+import {replaceContent} from '../../utils/domUtils';
+
+// Services
+import dbService from '../../services/dbService.js';
+
+const activeQuestion = function(wrapperEl, options ={}) {
+
+	const db = dbService.getDb();
+	const triggerClass = 'activequestion-trigger';
+	const defaults = {
+		onActiveQuestionChangeCallback: null
+	};
+	let settings = Object.assign({}, defaults, options);
+	let activeGameId;
+
+	function bindEvents() {
+		wrapperEl.addEventListener('change', onWrapperChange);
+	}
+
+	function onWrapperChange(e) {
+		const el = e.target;
+		if (el.classList.contains(triggerClass)) {
+			const val = el.value === 'no-value' ? false : parseInt(el.value);
+			updateDb(val);
+		}
+	}
+
+	function updateDb(val) {
+		db.ref(`games/${activeGameId}`).update({
+			activeQuestionId: val
+		});
+	}
+
+	async function render(gameId) {
+		if (activeGameId && !gameId) {
+			db.ref(`games/${activeGameId}`).update({
+				activeQuestionId: false
+			});
+		}
+		activeGameId = gameId;
+		const activeQuestionId = await db.ref(`games/${activeGameId}/activeQuestionId`).once('value').then(snapshot => snapshot.val());
+		const questions = await db.ref(`games/${activeGameId}/questions`).once('value').then(snapshot => snapshot.val());
+		let content = '';
+		if (questions) {
+			content = `
+				<label for="activeQuestion">Active Question:</label>
+				<select class="${triggerClass}" id="activeQuestion" name="activeQuestion">
+					${renderDropdownOptions(questions, activeQuestionId)}
+				</select>
+			`;
+		}
+		replaceContent(wrapperEl, content);
+	}
+
+	function renderDropdownOptions(questions, activeQuestionId) {
+		return Object.keys(questions).reduce((output, id) => {
+			return `
+				${output}
+				<option value="${id}" ${activeQuestionId === parseInt(id) ? 'selected' : ''}>${questions[id].question}</option>
+			`;
+		}, '<option value="no-value">No active question</option>');
+	}
+
+	bindEvents();
+
+	return {
+		render
+	};
+	
+};
+
+export default activeQuestion;
--- source/js/admin/screens/manager.js
+++ source/js/admin/screens/manager.js
@@ -4,20 +4,48 @@ import {replaceContent} from '../../utils/domUtils';
 // Components
 import killSwitch from '../components/killSwitch.js';
 import resetApp from '../components/resetApp.js';
+import activeEvent from '../components/activeEvent.js';
+import activeGame from '../components/activeGame.js';
+import activeQuestion from '../components/activeQuestion.js';
 
 const manager = function(el) {
 
 	const killSwitchWrapperClass = 'killswitch-wrapper';
 	const resetAppWrapperClass = 'resetapp-wrapper';
+	const activeEventWrapperClass = 'activeevent-wrapper';
+	const activeGameWrapperClass = 'activegame-wrapper';
+	const activeQuestionWrapperClass = 'activequestion-wrapper';
+	let activeGameInst;
+	let activeQuestionInst;
 
 	function render() {
 		replaceContent(el, `
 			<h1>Welcome, admin!</h1>
-			<div class="${killSwitchWrapperClass}"></div><br>
+			<div class="${activeEventWrapperClass}"></div>
+			<div class="${activeGameWrapperClass}"></div>
+			<div class="${activeQuestionWrapperClass}"></div>
+			<hr>
+			<div class="${killSwitchWrapperClass}"></div>
+			<hr>
 			<div class="${resetAppWrapperClass}"></div>
 		`);
 		killSwitch(el.querySelector(`.${killSwitchWrapperClass}`));
 		resetApp(el.querySelector(`.${resetAppWrapperClass}`));
+		activeEvent(el.querySelector(`.${activeEventWrapperClass}`), {
+			onActiveEventChangeCallback: onActiveEventChange
+		});
+		activeGameInst = activeGame(el.querySelector(`.${activeGameWrapperClass}`), {
+			onActiveGameChangeCallback: onActiveGameChange
+		});
+		activeQuestionInst = activeQuestion(el.querySelector(`.${activeQuestionWrapperClass}`));
+	}
+
+	function onActiveEventChange(activeEventId) {
+		activeGameInst.render(activeEventId);
+	}
+
+	function onActiveGameChange(activeGameId) {
+		activeQuestionInst.render(activeGameId);
 	}
 
 	return {
