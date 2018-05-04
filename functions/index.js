529e88def957994917e582a2082811c2d36381d3
Anna Scheuler <scheul93@gmail.com>
2018-05-04 10:23:32 -0500

    cloud functions for updating leaderboard

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,36 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+function updateLeaderboardScore(ref) {
+    return admin.database().ref(ref).transaction(currentVal => (currentVal || 0) + 1);
+}
+
+function updatePlayerScore(playerId, eventId, seriesId, gameId) {
+    if (playerId) {
+        const gameBoardRef = admin.database().ref(`leaderboardGame/${gameId}/${playerId}`);
+        const seriesBoardRef = admin.database().ref(`leaderboardSeries/${seriesId}/${playerId}`);
+        const eventBoardRef = admin.database().ref(`leaderboardEvent/${eventId}/${playerId}`);
+        const promises = [
+            updateLeaderboardScore(gameBoardRef),
+            updateLeaderboardScore(seriesBoardRef),
+            updateLeaderboardScore(eventBoardRef)
+        ];
+        return Promise.all(promises);
+    }
+}
+
+exports.updateLeaderboard = functions.database.ref('games/{gameId}/activeQuestionId')
+    .onWrite((change, context) => {
+        const questionId = change.before.val();
+        if (questionId){
+           return admin.database().ref(`answers/${questionId}`).once('value').then(snapshot => {
+               const questionRespData = snapshot.val();
+               const playersList = questionRespData.responses[questionRespData.correctChoice];
+               const promises = [];
+               Object.keys(playersList).forEach(playerId => {
+                   promises.push(updatePlayerScore(playerId, questionRespData.eventId, questionRespData.seriesId, questionRespData.gameId));
+               });
+               return Promise.all(promises);
+           })
+        }
+       return Promise.resolve();
+    })
