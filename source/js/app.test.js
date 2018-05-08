3394c52899b76dcd19549f627035118b51218be3
Ryan Heap <rheap@degdigital.com>
2018-05-08 11:42:12 -0500

    more app flow tests

--- source/js/app.test.js
+++ source/js/app.test.js
@@ -8,26 +8,92 @@ jest.mock('./services/eventsService');
 
 let appConfig;
 
-beforeEach(() => {
-	eventsService.__reset();
-
+beforeAll(() => {
 	const appEl = document.createElement('div');
 	appEl.setAttribute('id', 'app');
 	document.body.appendChild(appEl);
 
 	appConfig = getAppConfig();
+
+	app(appConfig);
 });
 
-describe('app', () => {
-	it('should route to registration page when user is unauthenticated', () => {
-        const routeSpy = jest.spyOn(router, 'route'); 
-        
-        app(appConfig);
+describe('app should route to', () => {
+	let routeSpy;
 
+	beforeAll(() => {
+		routeSpy = jest.spyOn(router, 'route');
+	});
+
+	beforeEach(() => {
+		jest.clearAllMocks(); 
+	});
+
+
+	test('registration page when user is unauthenticated', () => {
 		eventsService.__fireEvent('onPlayerUnauthenticated');
 
         expect(routeSpy).toHaveBeenCalledTimes(1);
         expect(routeSpy).toHaveBeenCalledWith('registration');
     });
 
+    test('info page when there is no active event', () => {
+    	const dataObj = {}; 
+    	eventsService.__fireEvent('onNoActiveEvent', dataObj);
+
+        expect(routeSpy).toHaveBeenCalledTimes(1);
+        expect(routeSpy).toHaveBeenCalledWith('info', dataObj);
+    });
+
+    test('pregame countdown page when there is an active event', () => {
+		eventsService.__fireEvent('onGameCountdown');
+
+        expect(routeSpy).toHaveBeenCalledTimes(1);
+        expect(routeSpy).toHaveBeenCalledWith('pregameCountdown');
+    });
+
+    test('game wait page when a game starts', () => {
+    	const dataObj = {}; 
+		eventsService.__fireEvent('onGameStart', dataObj);
+
+        expect(routeSpy).toHaveBeenCalledTimes(1);
+        expect(routeSpy).toHaveBeenCalledWith('gameWaitBeforeQuestions', dataObj);
+    });
+
+    test('question page when a question starts', () => {
+    	const dataObj = {}; 
+		eventsService.__fireEvent('onQuestionAsked', dataObj);
+
+        expect(routeSpy).toHaveBeenCalledTimes(1);
+        expect(routeSpy).toHaveBeenCalledWith('gameQuestion', dataObj);
+    });
+
+    test('question results page when a question ends', () => {
+    	const dataObj = {}; 
+		eventsService.__fireEvent('onBetweenQuestions', dataObj);
+
+        expect(routeSpy).toHaveBeenCalledTimes(1);
+        expect(routeSpy).toHaveBeenCalledWith('gameQuestionResults', dataObj);
+    });
+
+    test('game results page when game results are ready', () => {
+		eventsService.__fireEvent('onPostgameResults');
+
+        expect(routeSpy).toHaveBeenCalledTimes(1);
+        expect(routeSpy).toHaveBeenCalledWith('postgameResults');
+    });
+
+    test('pregame countdown page when a game ends', () => {
+		eventsService.__fireEvent('onGameEnd');
+
+        expect(routeSpy).toHaveBeenCalledTimes(1);
+        expect(routeSpy).toHaveBeenCalledWith('pregameCountdown');
+    });
+
+    test('error page when an error occurs', () => {
+		eventsService.__fireEvent('onError');
+
+        expect(routeSpy).toHaveBeenCalledTimes(1);
+        expect(routeSpy).toHaveBeenCalledWith('error');
+    });
 });
