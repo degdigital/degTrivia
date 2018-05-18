69ab83559c7b53d319d6c0276a1019da6a966930
Aaron Ladage <aladage@gmail.com>
2018-05-17 22:48:49 -0500

    Fixed admin issue with setting question to none

--- functions/components/setActiveQuestion.js
+++ functions/components/setActiveQuestion.js
@@ -3,12 +3,13 @@ module.exports = function(data, context, db, functions) {
 	const gameId = data.gameId;
 	const questionId = data.questionId;
 
-	if (!isValidIdFormat(gameId) || !isValidIdFormat(questionId)) {
-		throw new functions.https.HttpsError('invalid-argument', 'The function must be called with a valid gameId and questionId.');
-	}
-
-	if (isUnauthorizedUser()) {
-		throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+	function init() {
+		handleErrors();
+		if (questionId === false) {
+			return saveGameVals();
+		} else {
+			return getQuestionExpiration().then(questionExpirationTime => saveGameVals(questionId, questionExpirationTime));
+		}
 	}
 
 	function getQuestionExpiration() {
@@ -17,7 +18,7 @@ module.exports = function(data, context, db, functions) {
 		});
 	}
 
-	function saveGameVals(questionExpirationTime) {
+	function saveGameVals(questionId = false, questionExpirationTime = null) {
 		return db.ref(`/games/${gameId}`).update({
 			activeQuestionId: questionId,
 			questionExpirationTime: questionExpirationTime
@@ -28,16 +29,20 @@ module.exports = function(data, context, db, functions) {
 		return id && typeof id === 'string' && id.toString().length > 0;
 	}
 
-	function isUnauthorizedUser() {
-		return !context.auth;
+	function isAuthorizedUser() {
+		return context.auth;
 	}
 
-	return new Promise((resolve, reject) => {
-		getQuestionExpiration()
-			.then(questionExpirationTime => saveGameVals(questionExpirationTime))
-			.then(() => resolve({
-				success: true
-			}));
-	});
+	function handleErrors() {
+		if (!isValidIdFormat(gameId)) {
+			throw new functions.https.HttpsError('invalid-argument', 'The function must be called with a valid gameId.');
+		}
+
+		if (!isAuthorizedUser()) {
+			throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+		}
+	}
+
+	init();
 
 };
