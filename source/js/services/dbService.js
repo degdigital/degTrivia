e0d63b01f9da7dea5ae068a7b660e67ce34d10a6
Anna Scheuler <scheul93@gmail.com>
2018-05-21 16:03:22 -0500

    corrected property name

--- source/js/services/dbService.js
+++ source/js/services/dbService.js
@@ -121,7 +121,7 @@ const dbService = function() {
 
 	async function getPlayerScore(playerId) {
 		const recentGameId = await getMostRecentGameId();
-		return db.ref(`playerResultsGame/${recentGameId}/${playerId}`).once('value').then(snapshot => snapshot.val() || 0);
+		return db.ref(`playerResultsGame/${recentGameId}/${playerId}`).once('value').then(snapshot => (snapshot.val() && snaphot.val().score) || 0);
 	}
 
 	return {
