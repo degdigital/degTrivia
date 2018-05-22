80e916fafdfccf6f392ef7a12b84e8166cc1b70f
Aaron Ladage <aladage@gmail.com>
2018-05-22 13:00:06 -0500

    updated form to match new input js

--- source/js/screens/registration.js
+++ source/js/screens/registration.js
@@ -9,48 +9,48 @@ import formErrorMessage from '../components/forms/formErrorMessage.js';
 const registationFormAttr = 'data-registration-form';
 
 const registration = function({element}) {
-    
+
     function bindEvents() {
         element.addEventListener('submit', onFormSubmit);
     }
-    
+
     function render({errorMessage=null}) {
-        
+
         replaceContent(element, `
-                       <h1 class="page-title page-title--small">Welcome to DEG Trivia!</h1>
-                       <form ${registationFormAttr}>
-                       ${formErrorMessage({errorMessage})}
-                       <div class="field">
-                       ${label({content: 'First Name', inputId: 'firstName'})}
-                       ${textInput({id: 'firstName', isRequired: true, additionalAttrs:{autofocus: true}})}
-                       </div>
-                       <div class="field">
-                       ${label({content: 'Last Name', inputId: 'lastName'})}
-                       ${textInput({id: 'lastName', isRequired: true})}
-                       </div>
-                       <div class="field">
-                       ${label({content: 'Company', inputId: 'company'})}
-                       ${textInput({id: 'company', isRequired: true})}
-                       </div>
-                       <div class="field">
-                       ${label({content: 'Company Email', inputId: 'email'})}
-                       ${textInput({id: 'email', type: 'email', isRequired: true})}
-                       </div>
-                       <div class="field">
-                       ${label({content: 'Phone', inputId: 'phoneNumber'})}
-                       ${textInput({id: 'phoneNumber', type: 'tel', isRequired: true})}
-                       </div>
-                       <div class="field">
-                       ${label({content: 'Event Code', inputId: 'event'})}
-                       ${textInput({id: 'event', isRequired: true, additionalAttrs:{autocapitalize: 'off'}})}
-                       </div>
-                       <div class="button-group button-group--centered">
-                       ${button({content: 'Let\'s Play'})}
-                       </div>
-                       </form>
-                       `);
+            <h1 class="page-title page-title--small">Welcome to DEG Trivia!</h1>
+            <form ${registationFormAttr}>
+                ${formErrorMessage({errorMessage})}
+                <div class="field">
+                    ${label({content: 'First Name', inputId: 'firstName'})}
+                    ${textInput({id: 'firstName', isRequired: true, additionalAttrs:{autofocus: true}})}
+                </div>
+                <div class="field">
+                    ${label({content: 'Last Name', inputId: 'lastName'})}
+                    ${textInput({id: 'lastName', isRequired: true})}
+                </div>
+                <div class="field">
+                    ${label({content: 'Company', inputId: 'company'})}
+                    ${textInput({id: 'company', isRequired: true})}
+                </div>
+                <div class="field">
+                    ${label({content: 'Company Email', inputId: 'email'})}
+                    ${textInput({id: 'email', type: 'email', isRequired: true})}
+                </div>
+                <div class="field">
+                    ${label({content: 'Phone', inputId: 'phoneNumber'})}
+                    ${textInput({id: 'phoneNumber', type: 'tel', isRequired: true, pattern: '[0-9-+\s()]*$', additionalAttrs:{minlength: '10', maxlength: '14', placeholder: 'XXX-XXX-XXXX'}})}
+                </div>
+                <div class="field">
+                    ${label({content: 'Event Code', inputId: 'event'})}
+                    ${textInput({id: 'event', isRequired: true, additionalAttrs:{autocapitalize: 'off'}})}
+                </div>
+                <div class="button-group button-group--centered">
+                    ${button({content: 'Let\'s Play'})}
+                </div>
+            </form>
+        `);
     }
-    
+
     function onFormSubmit(e) {
         const el = e.target;
         if (el.hasAttribute(registationFormAttr)) {
@@ -59,23 +59,23 @@ const registration = function({element}) {
             playerService.register(formVals)
             .then(successMsg => renderPostRegisterMessage(element, successMsg))
             .catch(errorMsg => {
-                   renderPostRegisterMessage(errorPlaceholderEl, errorMsg)
-                   });
+                renderPostRegisterMessage(errorPlaceholderEl, errorMsg)
+            });
         }
     }
-    
+
     function renderPostRegisterMessage(el, message) {
         replaceContent(el, `
-                       <p>${message}</p>
-                       `);
+            <p>${message}</p>
+        `);
     }
-    
+
     bindEvents();
-    
+
     return {
-    render: () => render({})
+        render: () => render({})
     };
-    
+
 };
 
 export default registration;
