fa96cb9e3db314fc9a0a4b18cca71fc46eb2a364
Ryan Heap <rheap@degdigital.com>
2018-05-22 12:40:30 -0500

    added pattern attr to text input

--- source/js/components/forms/textInput.js
+++ source/js/components/forms/textInput.js
@@ -2,7 +2,7 @@ import classnames from 'classnames';
 import { renderHtmlAttrs } from '../../utils/htmlAttrs.js';
 
 
-export default function textInput({id, type='text', value='', isInvalid=false, isRequired=false, additionalAttrs={}}) {
+export default function textInput({id, type='text', value='', isInvalid=false, isRequired=false, pattern='', additionalAttrs={}}) {
 	const classes = classnames('input', {
 		'input--invalid': isInvalid
 	});
@@ -14,5 +14,6 @@ export default function textInput({id, type='text', value='', isInvalid=false, i
 		type="${type}" 
 		value="${value}" 
 		${isRequired ? 'required' : ''} 
+		${pattern ? `pattern="${pattern}"` : ''}
 		${renderHtmlAttrs(additionalAttrs)} />`;
 }
