077dd50021acc6eee135bca89e8efd3564322792
Anna Scheuler <scheul93@gmail.com>
2018-05-29 09:40:37 -0500

    integration into app

--- source/js/screens/gameWaitBeforeQuestions.js
+++ source/js/screens/gameWaitBeforeQuestions.js
@@ -2,14 +2,53 @@ import {replaceContent} from '../utils/domUtils.js';
 
 const gameWaitBeforeQuestions = function(element) {
 
+	const siteHeaderSelector = '[data-site-header]';
+	const hiddenClass = 'is-hidden';
+
+	function hideSiteHeader() {
+		const siteHeaderEl = document.querySelector(siteHeaderSelector);
+		if(siteHeaderEl) {
+			siteHeaderEl.classList.add(hiddenClass);
+		}
+	}
+
 	function render() {
+		hideSiteHeader();
 		replaceContent(element, `
-			Game has started! Waiting for the first question...
+			<div class="background__video-wrap">
+				<video class="background__video" 
+					autoplay 
+					loop 
+					muted
+					playsinline
+					preload="auto" 
+					poster="../../images/App-Hex-Ani-Congratulations.jpg"
+					type="video/mp4"
+				>
+					<source src="../../images/app-hex-ani-375.mp4" type="video/mp4">
+				</video>
+			</div>
+			<div class="game-begin__content">
+				<div class="game-begin-heading">
+					<img src="../../images/deg-logo.svg" alt="DEG logo" class="logo" />
+					<h1 class="page-title page-title--centered">Let's Play!</h1>
+					<p class="hashtag text--centered">#CNXTRIVIA</p>
+				</div>
+				<p class="subheading text--centered">Answer all six questions (not stopping for wrong answers) as fast as you can. 10 seconds per question. Speed and accuracy wins. Here comes the first question.</p>
+			</div>
 		`);
 	}
 
+	function teardown() {
+		const siteHeaderEl = document.querySelector(siteHeaderSelector);
+		if(siteHeaderEl) {
+			siteHeaderEl.classList.remove(hiddenClass);
+		}
+	}
+
 	return {
-		render
+		render,
+		teardown
 	};
 
 };
