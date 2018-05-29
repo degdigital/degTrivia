8b35118901c45a5084a47bb187fcc415059c9e96
Aaron Ladage <aladage@gmail.com>
2018-05-29 09:41:12 -0500

    re-added formatter function and rule check

--- source/js/services/playerService.js
+++ source/js/services/playerService.js
@@ -55,12 +55,21 @@ const playerService = function() {
 			firstName: playerVals.firstName,
 			lastName: playerVals.lastName,
 			companyName: playerVals.companyName,
-			phoneNumber: playerVals.phoneNumber,
+			phoneNumber: formatPhoneNumber(playerVals.phoneNumber),
 			email: playerVals.email,
 			event: eventKey,
 			active: true
 		};
 		return dbService.getDb().ref(`/players/${uid}`).update(formattedPlayerVals);
+	}
+
+	function formatPhoneNumber(str = '') {	
+		const strWithSpecialCharsRemoved = str.replace(/\D/g, '');	
+		if (strWithSpecialCharsRemoved.length === 10) {	
+			return `1${strWithSpecialCharsRemoved}`;	
+		} else {	
+			return strWithSpecialCharsRemoved;	
+		}	
 	}
 
 	function authorize(eventAlias) {
