6c76bd7431a87cf4ee44373e73eb80b41dbc5a24
Aaron Ladage <aladage@gmail.com>
2018-05-30 12:07:20 -0500

    added login

--- source/js/leaderboard/index.js
+++ source/js/leaderboard/index.js
@@ -4,26 +4,43 @@ import firebaseConfig from '../config/firebaseConfig.js';
 
 // Services
 import dbService from '../services/dbService.js';
+import playerService from '../services/playerService.js';
 
 // Screens
+import login from '../admin/screens/login.js';
 import manager from './screens/manager.js';
 
 const leaderboardTV = function(el) {
 
 	let db;
 	let loginInst;
+	let managerInst;
 
 	function setUpListeners(managerInst) {
-		dbService.getDb().ref(`leaderboardCurrent`).on('value', snapshot => managerInst.render(snapshot.val()));
+		playerService.getAuth().onAuthStateChanged(onAuthStateChange);
+	}
+
+	async function onAuthStateChange(user) {
+		if (user) {
+			const isAdmin = await db.ref(`admins/${user.uid}`).once('value').then(snapshot => snapshot.exists() && snapshot.val() === true);
+			if (isAdmin === true) {
+				dbService.getDb().ref(`leaderboardCurrent`).on('value', snapshot => managerInst.render(snapshot.val()));
+			} else {
+				loginInst.renderForm();
+			}
+		} else {
+			loginInst.renderForm();
+		}
 	}
 
 	function init() {
 		firebase.initializeApp(firebaseConfig);
 		dbService.init();
+		playerService.init();
 
 		db = dbService.getDb();
-		const managerInst = manager(el);
-		managerInst.render();
+		loginInst = login(el);
+		managerInst = manager(el);
 		
 		setUpListeners(managerInst);
 	}
