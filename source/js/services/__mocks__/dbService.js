837bb84badfb0d17cb2dbf35b5f5dcfd0e3060d3
Anna Scheuler <scheul93@gmail.com>
2018-06-04 10:11:32 -0500

    test updates

--- source/js/services/__mocks__/dbService.js
+++ source/js/services/__mocks__/dbService.js
@@ -9,14 +9,14 @@ function submitAnswer() {
 function getLeaderboardData() {
 	return Promise.resolve({
 		game: [
-			{name: 'Anna', score: '1500000'},
-			{name: 'Aaron', score: '140'},
-			{name: 'Ryan', score: '42'}
+			{name: 'Anna', score: '1500000', timeElapsed: '8103'},
+			{name: 'Aaron', score: '140', timeElapsed: '81030'},
+			{name: 'Ryan', score: '42', timeElapsed: '72030'}
 		],
 		event: [
-			{name: 'Aaron', score: '1500001'},
-			{name: 'Aaron', score: '200'},
-			{name: 'Ryan', score: '43'}
+			{name: 'Aaron', score: '1500001', timeElapsed: '8103'},
+			{name: 'Aaron', score: '200', timeElapsed: '81030'},
+			{name: 'Ryan', score: '43', timeElapsed: '72030'}
 		]
 	})
 }
